1ec65542524e94a8e87092300f3ebf91b1c2501a
Ricardo Marques <sevendulac@gmail.com>
2020-09-17 17:05:57 -0300

    Exercice Rails Yelp MVP Evolued

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    get 'top', on: :collection # get 'restaurants/top', to: 'restaurants#top'
+    get 'chef', on: :member # get 'restaurants/:id/chef', to: 'restaurants#chef'
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews, only: [:destroy]
+
+
+
+  root to: 'restaurants#index'
 end
